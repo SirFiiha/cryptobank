ee980b8eac54863bbcdba8d087b1e6328f4e0ef1
Branko Visser <b.visser@kwigy.com>
2021-09-22 13:58:21 +0200

    structuur aangepast

--- src/main/java/com/example/cryptobank/security/CreateToken.java
+++
@@ -1,8 +0,0 @@
-package com.example.cryptobank.security;
-
-public interface CreateToken {
-
-    String createAccessToken(String username, String key);
-    String createRefreshToken(String username, String key);
-    String createResetToken(String username, String key, String salt);
-}
